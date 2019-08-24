15ea2df392430b4d0842c7afef2a3291807be726
Protopopova <Alina.Protopopova@billing.ru>
2019-08-24 15:27:10 +1000

    Integrated Reactjs

---
+++ WebApplication1/WebApplication1/App_Start/ReactConfig.cs
@@ -0,0 +1,18 @@
+using React;
+using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.V8;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(WebApplication1.ReactConfig), "Configure")]
+
+namespace WebApplication1
+{
+	public static class ReactConfig
+	{
+		public static void Configure()
+		{
+            // Регистрация движка V8
+            JsEngineSwitcher.Current.DefaultEngineName = V8JsEngine.EngineName;
+            JsEngineSwitcher.Current.EngineFactories.AddV8();
+        }
+	}
+}
